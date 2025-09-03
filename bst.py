ec91309403195d55ed5ae9659375b322f09d2c00
HYPER_77 <143863603+Hyper7711@users.noreply.github.com>
2025-09-03 19:19:53 +0530

    update_bst

--- bst.py
+++ bst.py
@@ -1,10 +1,11 @@
 class Node:
     def __init__(self, key):
-        self.left = self.right = None
+        self.left = None
+        self.right = None
         self.val = key
 
 def insert(root, key):
-    if not root:
+    if root is None:
         return Node(key)
     if key < root.val:
         root.left = insert(root.left, key)
@@ -13,14 +14,16 @@ def insert(root, key):
     return root
 
 def inorder(root):
-    if root:
+    if root is not None:
         inorder(root.left)
         print(root.val, end=' ')
         inorder(root.right)
 
-# Example usage
-r = Node(50)
-for k in [30, 20, 40, 70, 60, 80]:
-    insert(r, k)
-inorder(r)
+root = Node(50)
+keys = [30, 20, 40, 70, 60, 80]
 
+for key in keys:
+    insert(root, key)
+
+inorder(root)
+print()
